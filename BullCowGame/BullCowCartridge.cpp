ad4b29c77f70f8959936f4837040ff73c9fda63c
Riley Stange <riley.stange@gmail.com>
2019-12-08 19:49:00 -0600

    Added Win/Lose conditions

--- BullCowGame/BullCowCartridge.cpp
+++ BullCowGame/BullCowCartridge.cpp
@@ -6,12 +6,23 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Bull Cows."));
     PrintLine(TEXT("Are you smarter than these cows?"));
+    PrintLine(TEXT("Guess the 5 letter word.")); // Magic Number Remove!
     PrintLine(TEXT("Press enter to begin..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("bugle");
+    FString HiddenWord = TEXT("bugle"); // Move outside of the function later.
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("Correct! You win!"));
+    }
+    else
+    {
+        PrintLine(TEXT("Sorry, that is incorrect!"));
+        PrintLine(TEXT("Game over!"));
+    }
+    
     PrintLine(Input);
 }
