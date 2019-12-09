880a097b3a0d6abc2b38d86a303322bffb102975
Riley Stange <riley.stange@gmail.com>
2019-12-08 18:05:57 -0600

    Initial Commit: Testing out the cartridge

---
+++ BullCowGame/BullCowCartridge.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BullCowCartridge.h"
+
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+    Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Bull Cows."));
+    PrintLine(TEXT("Are you smarter than these cows?"));
+    PrintLine(TEXT("Press enter to begin..."));
+}
+
+void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+{
+    ClearScreen();
+    FString HiddenWord = TEXT("bugle");
+}
