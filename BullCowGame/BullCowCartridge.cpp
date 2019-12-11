cbb89d6e7242d5415acb5e2dd86df8a12dddce61
Riley Stange <riley.stange@gmail.com>
2019-12-10 20:30:59 -0600

    Added EndGame function and bGameOver boolean to to track if Game is over Moved welcome messages to SetupGame

--- BullCowGame/BullCowCartridge.cpp
+++ BullCowGame/BullCowCartridge.cpp
@@ -6,46 +6,44 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     Super::BeginPlay();
 
     SetupGame();
-
-    PrintLine(FString::Printf(TEXT("The hidden word is: %s"), *HiddenWord)); // Debug line
-    
-    // Welcome message
-    PrintLine(TEXT("Welcome to Bull Cows."));
-    PrintLine(TEXT("Are you smarter than these cows?"));
-    PrintLine(FString::Printf(TEXT("Enter the %i letter word."), HiddenWord.Len()));
-    PrintLine(TEXT("And then press enter..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();
-    // Check Right Amount of Characters
-        // If wrong amount, guess again
-    if (Input.Len() != HiddenWord.Len() )
+    if (bGameOver)
     {
-        PrintLine(TEXT("The hidden word is %i letters long, try again!"), HiddenWord.Len());
+        ClearScreen();
+        SetupGame();
     }
-    // Check if input has no repeating characters
-        // If input has repeating characters, guess again
-    // If Correct
-        // Display Win message
-    else
+    else // Check the player guess
     {
-        if (Input == HiddenWord)
+        // Check Right Amount of Characters
+            // If wrong amount, guess again
+        if (Input.Len() != HiddenWord.Len() )
         {
-            PrintLine(TEXT("Correct! You win!"));
+            PrintLine(TEXT("The hidden word is %i letters long\nTry again!"), HiddenWord.Len());
         }
-        // If Lives is == 0, 
-            // Game Over
-            // Reveal HiddenWord
-
-        // If lives > 0 
-            // Reveal Remaining Lives
-            // Ask Again.
+        // Check if input has no repeating characters
+            // If input has repeating characters, guess again
         else
         {
-            PrintLine(TEXT("Sorry, that is incorrect!")); 
-            PrintLine(TEXT("Game over!")); 
+            if (Input == HiddenWord)
+            {
+                PrintLine(TEXT("Correct! You win!"));
+                EndGame();
+            }
+            else
+            // If Lives is == 0, 
+                // Game Over
+                // Reveal HiddenWord
+
+            // If lives > 0 
+                // Reveal Remaining Lives
+                // Ask Again.
+            {
+                PrintLine(TEXT("Sorry, that is incorrect!")); 
+                EndGame();
+            }
         }
     }
     // After win or lose:
@@ -56,8 +54,22 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
 
 void UBullCowCartridge::SetupGame()
 {
+    bGameOver = false;
     HiddenWord = TEXT("cake");
-    
+
     // Set lives == the length of the HiddenWord string.
     Lives = HiddenWord.Len(); 
+    // bGameOver = false;
+    PrintLine(FString::Printf(TEXT("The hidden word is: %s"), *HiddenWord)); // Debug line
+    
+    // Welcome message
+    PrintLine(TEXT("Welcome to Bull Cows."));
+    PrintLine(TEXT("Are you smarter than these cows?"));
+    PrintLine(FString::Printf(TEXT("Enter the %i letter word.\nPress enter to continue..."), HiddenWord.Len()));
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("Game over! Press Enter to Continue..."));
 }
