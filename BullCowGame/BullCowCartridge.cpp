7b1d0b62f9dab07a7781f73b219ca54e29d00903
Riley Stange <riley.stange@gmail.com>
2019-12-10 19:13:58 -0600

    Added check for length of input compared to HiddenWord

--- BullCowGame/BullCowCartridge.cpp
+++ BullCowGame/BullCowCartridge.cpp
@@ -1,15 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
-
-
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     // Welcome message
     PrintLine(TEXT("Welcome to Bull Cows."));
     PrintLine(TEXT("Are you smarter than these cows?"));
-    PrintLine(TEXT("Enter the 5 letter word.")); // Magic Number Remove!
+    PrintLine(TEXT("Enter the 5 letter word.")); // Magic Number
     PrintLine(TEXT("And then press enter..."));
     
     SetupGame();
@@ -18,30 +16,35 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    // Check if input has no repeating characters
-        // If input has repeating characters, guess again
     // Check Right Amount of Characters
         // If wrong amount, guess again
-    
+    if (Input.Len() != HiddenWord.Len() )
+    {
+        PrintLine(TEXT("The hidden word is 5 letters long, try again!")); // Magic Number
+    }
+    // Check if input has no repeating characters
+        // If input has repeating characters, guess again
     // If Correct
         // Display Win message
-    if (Input == HiddenWord)
-    {
-        PrintLine(TEXT("Correct! You win!"));
-    }
-    // If Lives is == 0, 
-        // Game Over
-        // Reveal HiddenWord
-
-    // If lives > 0 
-        // Reveal Remaining Lives
-        // Ask Again.
     else
     {
-        PrintLine(TEXT("Sorry, that is incorrect!")); 
-        PrintLine(TEXT("Game over!")); 
-    }
+        if (Input == HiddenWord)
+        {
+            PrintLine(TEXT("Correct! You win!"));
+        }
+        // If Lives is == 0, 
+            // Game Over
+            // Reveal HiddenWord
 
+        // If lives > 0 
+            // Reveal Remaining Lives
+            // Ask Again.
+        else
+        {
+            PrintLine(TEXT("Sorry, that is incorrect!")); 
+            PrintLine(TEXT("Game over!")); 
+        }
+    }
     // After win or lose:
     // Ask player if they want to play again
     // If Yes, go back to BeginPlay
@@ -51,5 +54,7 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
 void UBullCowCartridge::SetupGame()
 {
     HiddenWord = "bugle";
-    Lives = 5; // Set lives == the length of the HiddenWord string.
+    
+    // Set lives == the length of the HiddenWord string.
+    Lives = HiddenWord.Len(); 
 }
